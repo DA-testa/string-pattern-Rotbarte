b5615c0c773e5f79e58ebf74edca8892ddaf2c07
Katerina Rotbarte 221RDB292 <125587202+Rotbarte@users.noreply.github.com>
2023-04-16 23:58:40 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -19,24 +19,30 @@ def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-   
     prime = 10**9 + 7
     x = 263
     pattern_len = len(pattern)
     text_len = len(text)
     pattern_hash = 0
-    text_hashes = []
     h = 1
     for i in range(pattern_len):
-        pattern_hash = (pattern_hash * x + ord(pattern[i])) % prime
+        pattern_hash = (pattern_hash + ord(pattern[i]) * h) % prime
         h = (h * x) % prime
+    text_hashes = []
+    h = 1
     for i in range(text_len - pattern_len + 1):
-        text_hash = 0
-        for j in range(pattern_len):
-            text_hash = (text_hash * x + ord(text[i+j])) % prime
-        if text_hash == pattern_hash and text[i:i+pattern_len] == pattern:
-            occurrences.append(i)
+        if i == 0:
+            for j in range(pattern_len):
+                text_hashes.append((ord(text[pattern_len - 1 - j]) * h) % prime)
+                if j < pattern_len - 1:
+                    h = (h * x) % prime
+        else:
+            prev_h = (ord(text[i - 1]) * h) % prime
+            new_h = (ord(text[i + pattern_len - 1]) * x) % prime
+            text_hashes[i] = (text_hashes[i - 1] - prev_h + new_h) % prime
+    occurrences = [i for i in range(text_len - pattern_len + 1) if pattern_hash == text_hashes[i]]
     return occurrences
+
 
 
 
