034aa9eb7d4fbdb0d233adf2afb7d27f3365fc42
Katerina Rotbarte 221RDB292 <125587202+Rotbarte@users.noreply.github.com>
2023-04-17 00:15:05 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -20,23 +20,23 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     occurrences = []
-    patternLength = len(pattern)
+    lenpattern = len(pattern)
     
     prime = 10**9+7
     d = 256
-    h = pow(d, patternLength-1, prime)
+    h = pow(d, lenpattern-1, prime)
     patternHashVal = 0
     textHashVal = 0
-    for i in range(patternLength):
+    for i in range(lenpattern):
         patternHashVal = (patternHashVal*d + ord(pattern[i])) % prime
         textHashVal = (textHashVal*d + ord(text[i])) % prime
 
     if patternHashVal == textHashVal and pattern == text[:patternLength]:
         occurrences.append(0)
 
-    for i in range(1, len(text) - patternLength + 1):
-        textHashVal = (d*(textHashVal-ord(text[i-1])*h) + ord(text[i+patternLength-1])) % prime
-        if patternHashVal == textHashVal and pattern == text[i:i+patternLength]:
+    for i in range(1, len(text) - lenpattern + 1):
+        textHashVal = (d*(textHashVal-ord(text[i-1])*h) + ord(text[i+lenpattern-1])) % prime
+        if patternHashVal == textHashVal and pattern == text[i:i+lenpattern]:
             occurrences.append(i)
 
     return occurrences
