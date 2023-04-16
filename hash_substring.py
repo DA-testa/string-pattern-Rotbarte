6c5cb04f4b2121493ebd83e40e0fa2f15b16b7db
Katerina Rotbarte 221RDB292 <125587202+Rotbarte@users.noreply.github.com>
2023-04-16 23:31:29 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -15,22 +15,22 @@ def read_input():
     
     return (input().rstrip(), input().rstrip())
 
-def print_occurrences(output):
-    print(' '.join(map(str, output)))
-   
-def hash_func(s, prime=10**9 + 7, x=263):
-    y= 0
+def hash_func(s, prime, x):
+    h = 0
     for c in reversed(s):
-        y= (y * x + ord(c)) % prime
-    return y
+        h = (h * x + ord(c)) % prime
+    return h
 
 def get_occurrences(pattern, text):
-    pattern_len = len(pattern)
-    text_len = len(text)
-    pattern_hash = hash_func(pattern)
-    text_hashes = [hash_func(text[i:i+pattern_len]) for i in range(text_len-pattern_len+1)]
-    occurances= [i for i in range(text_len-pattern_len+1) if text_hashes[i] == pattern_hash]
-    return occurances
+    # this function should find the occurrences using Rabin Karp algorithm
+    prime = 10**9 + 7
+    x = 263
+    p_len = len(pattern)
+    t_len = len(text)
+    p_hash = hash_func(pattern, prime, x)
+    t_hashes = [hash_func(text[i:i+p_len], prime, x) for i in range(t_len-p_len+1)]
+    occurrences = [i for i in range(t_len-p_len+1) if t_hashes[i] == p_hash]
+    return occurrences
  
 
 
