b30cebfbd5ef4b64f59b6556a5935d01ae7c2a22
Katerina Rotbarte 221RDB292 <125587202+Rotbarte@users.noreply.github.com>
2023-04-16 23:39:21 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,7 +1,6 @@
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+ 
     example = input()
     if "I" in example:
         pattern = input()
@@ -13,15 +12,10 @@ def read_input():
             text = file.readline().rstrip()
     
     return (pattern.rstrip(), text.rstrip())
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    # return both lines in one return
-    # this is the sample return, notice the rstrip function
+
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
+    
     print(' '.join(map(str, output)))
 
 def hash_func(s, prime, x):
@@ -31,7 +25,7 @@ def hash_func(s, prime, x):
     return h
 
 def get_occurrences(pattern, text):
-    # this function should find the occurrences using Rabin Karp algorithm
+    
     prime = 10**9 + 7
     x = 263
     p_len = len(pattern)
@@ -41,6 +35,6 @@ def get_occurrences(pattern, text):
     occurrences = [i for i in range(t_len-p_len+1) if t_hashes[i] == p_hash]
     return occurrences
 
-# this part launches the functions
+
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
