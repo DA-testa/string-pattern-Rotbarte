0d9b00677a8a580870264bdac982ef390446e20b
Katerina Rotbarte 221RDB292 <125587202+Rotbarte@users.noreply.github.com>
2023-04-16 23:35:07 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,6 +1,7 @@
 
-
 def read_input():
+    # this function needs to aquire input both from keyboard and file
+    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     choice = input()
     if "I" in choice:
         pattern = input()
@@ -12,6 +13,16 @@ def read_input():
             text = files.readline()
     
     return (pattern.rstrip(), text.rstrip())
+    # after input type choice
+    # read two lines 
+    # first line is pattern 
+    # second line is text in which to look for pattern 
+    # return both lines in one return
+    # this is the sample return, notice the rstrip function
+
+def print_occurrences(output):
+    # this function should control output, it doesn't need any return
+    print(' '.join(map(str, output)))
 
 def hash_func(s, prime, x):
     h = 0
@@ -29,9 +40,7 @@ def get_occurrences(pattern, text):
     t_hashes = [hash_func(text[i:i+p_len], prime, x) for i in range(t_len-p_len+1)]
     occurrences = [i for i in range(t_len-p_len+1) if t_hashes[i] == p_hash]
     return occurrences
- 
 
-
+# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
