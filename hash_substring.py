2da10454cb609f6b926540e6ec2fed9ac8177eed
Katerina Rotbarte 221RDB292 <125587202+Rotbarte@users.noreply.github.com>
2023-04-17 00:00:51 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -18,31 +18,20 @@ def print_occurrences(output):
     
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    prime = 10**9 + 7
-    x = 263
+def hash_func(s, z=10**9 + 7, x=263):
+    y= 0
+    for c in reversed(s):
+        y= (y * x + ord(c)) % z
+    return y
+
+
+def get_occurrences(pattern, text, z=10**9 + 7, x=263):
     pattern_len = len(pattern)
     text_len = len(text)
-    pattern_hash = 0
-    h = 1
-    for i in range(pattern_len):
-        pattern_hash = (pattern_hash + ord(pattern[i]) * h) % prime
-        h = (h * x) % prime
-    text_hashes = []
-    h = 1
-    for i in range(text_len - pattern_len + 1):
-        if i == 0:
-            for j in range(pattern_len):
-                text_hashes.append((ord(text[pattern_len - 1 - j]) * h) % prime)
-                if j < pattern_len - 1:
-                    h = (h * x) % prime
-        else:
-            prev_h = (ord(text[i - 1]) * h) % prime
-            new_h = (ord(text[i + pattern_len - 1]) * x) % prime
-            text_hashes[i] = (text_hashes[i - 1] - prev_h + new_h) % prime
-    occurrences = [i for i in range(text_len - pattern_len + 1) if pattern_hash == text_hashes[i]]
+    pattern_hash = hash_func(pattern, z, x)
+    text_hashes = [hash_func(text[i:i+pattern_len], z, x) for i in range(text_len-pattern_len+1)]
+    occurrences = [i for i in range(text_len-pattern_len+1) if text_hashes[i] == pattern_hash]
     return occurrences
-
 
 
 
